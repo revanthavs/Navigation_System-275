7dd2d66ade48051ef6f1260c2cb3ae693b6d0c6c
REVANTHAVS <atmakurirevanth@gamil.com>
2021-03-21 07:16:05 -0600

    Heap insert & min methods (21-03-2021)

--- part1_starter/heap.h
+++ part1_starter/heap.h
@@ -36,4 +36,45 @@ private:
   // will fix the heap property at index i and recurse with the child
   // that received i's item (if appropriate)
   void fixHeapDown(int i);
+
+  // Returns the parent index of the given index
+  unsigned int parent_index(unsigned int i);
 };
+
+/*
+  R: Returns the pair (x, key) with the smallest key
+*/
+HeapItem<T, K> BinaryHeap::min() const{
+  if (heap.size() > 0){
+    // R: Since the first element of the array holding the heap is the root index
+    // R: Which holds the minimum key value
+    return heap[0];
+  }
+}
+
+void BinaryHeap::insert(const T& item, const K& key){
+  HeapItem<T, K> temp_v;
+  temp_v.item = item;
+  temp_v.key = key;
+  heap.push_back(temp_v);
+  //R: This code need to be implemented in fixheapup
+  unsigned int v_i = heap.size() - 1;
+  unsigned int p_i = parent_index(v_i);
+  while ((v_i != 0) && (heap[v_i].key < heap[p_i].key)){
+    swap(heap[v_i], heap[p_i]);
+    v_i = p_i;
+    p_i = parent_index(v_i);
+  }
+}
+
+int BinaryHeap::size() const{
+  return heap.size();
+}
+
+unsigned int BinaryHeap::parent_index(unsigned int i){
+  if (i > 0){
+    return ((i-1)/2);
+  }
+  else
+    return 0;
+}
