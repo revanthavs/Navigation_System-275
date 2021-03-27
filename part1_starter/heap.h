f7f620cf59fe77d70ac1c99b55a0d202c202d1a6
REVANTHAVS <atmakurirevanth@gamil.com>
2021-03-27 15:07:12 -0600

    copy version of implementation

--- part1_starter/heap.h
+++ part1_starter/heap.h
@@ -1,4 +1,5 @@
 #include <vector>
+using namespace std; // need to remove this
 
 // Only assumes the key type K is totally ordered and comparable via <
 template <class T, class K>
@@ -19,6 +20,9 @@ public:
   // insert an item with the given key
   // if the item is already in the heap, will still insert a new copy with this key
   void insert(const T& item, const K& key);
+
+// need to remove
+  void insert(const T& item, const K& key, vector<HeapItem<T,K>>& heapcopy, const unsigned int heapsize);
 
   // pop the minimum item from the heap
   void popMin();
@@ -44,7 +48,7 @@ private:
   void fixHeapDown(int i);
 
   // R: Returns the parent index of the given index (Helper method)
-  // unsigned int parent_index(unsigned int i);
+  unsigned int parent_index(unsigned int i);
 };
 
 /*
@@ -77,6 +81,17 @@ void BinaryHeap<T, K>::fixHeapUp(int i){
         return;
 }
 
+
+// template <class T, class K>
+// unsigned int BinaryHeap<T, K>::parent_index(unsigned int i){
+//   if (i > 0){
+//     // R: Since we are storing all the values in array
+//     return ((i-1)/2);
+//   }
+//   else
+//     return 0;
+// }
+
 template <class T, class K>
 void BinaryHeap<T, K>::insert(const T& item, const K& key){
   HeapItem<T, K> temp_v;  // Need to update this approach
@@ -100,56 +115,46 @@ int BinaryHeap<T, K>::size() const{
 }
 
 // template <class T, class K>
-// unsigned int BinaryHeap<T, K>::parent_index(unsigned int i){
-//   if (i > 0){
-//     // R: Since we are storing all the values in array
-//     return ((i-1)/2);
-//   }
+// int BinaryHeap<T, K>::left(int parent){
+//   int left=2*parent+1;
+
+//   //J: if the left child has a valid index
+//   if (left < size())
+//     return left;
+//   //J: if the index of left child is out of bounds
 //   else
-//     return 0;
+//     return -1;
 // }
+// template <class T, class K>
+// int BinaryHeap<T, K>::right(int parent){
+//   int right=2*parent+1;
 
-template <class T, class K>
-int BinaryHeap<T, K>::left(int parent){
-	int left=2*parent+1;
-
-	//J: if the left child has a valid index
-	if (left < size())
-		return left;
-	//J: if the index of left child is out of bounds
-	else
-		return -1;
-}
-template <class T, class K>
-int BinaryHeap<T, K>::right(int parent){
-	int right=2*parent+1;
-
-	//J: if the right child has a valid index
-	if (right < size())
-		return right;
-	//J: if the index of right child is out of bounds
-	else
-		return -1;
-}
-template <class T, class K>
-void BinaryHeap<T,K>::fixHeapDown(int i)
-{
-	int child1=left(i);
-	int child2=right(i);
-	
-	//J: if both the children are valid indices and the right child has the minimum value
-	if(child1 >= 0 && child2 >= 0 && heap[child1].key > heap[child2].key)
-	{
-		child1=child2;
-	}
-	//J: if the minimum child is a valid index and the does not obey the heap property,swap
-	if (child1 > 0 && heap[i].key > heap[child1].key)
-	{
-		swap(heap[i], heap[child1]);
-		//J: checking if the swapped vertex obeys the heap property
-		fixHeapDown(child1);
-	}
-}
+//   //J: if the right child has a valid index
+//   if (right < size())
+//     return right;
+//   //J: if the index of right child is out of bounds
+//   else
+//     return -1;
+// }
+// template <class T, class K>
+// void BinaryHeap<T,K>::fixHeapDown(int i)
+// {
+//   int child1=left(i);
+//   int child2=right(i);
+  
+//   //J: if both the children are valid indices and the right child has the minimum value
+//   if(child1 >= 0 && child2 >= 0 && heap[child1].key > heap[child2].key)
+//   {
+//     child1=child2;
+//   }
+//   //J: if the minimum child is a valid index and the does not obey the heap property,swap
+//   if (child1 > 0 && heap[i].key > heap[child1].key)
+//   {
+//     swap(heap[i], heap[child1]);
+//     //J: checking if the swapped vertex obeys the heap property
+//     fixHeapDown(child1);
+//   }
+// }
 
   /*while (heap[i].key > heap[min_child].key)
   {
@@ -160,29 +165,264 @@ void BinaryHeap<T,K>::fixHeapDown(int i)
     swap(heap[i], heap[min_child]);
     i=min_child;
     left=2*i+1;
-  	right=2*i+2;
+    right=2*i+2;
 
-	// //J: has no children
-	// if(left> size()-1)  break;
+  // //J: has no children
+  // if(left> size()-1)  break;
 
-	//J: has left child only
-	if(left < (size()-1) && right > (size()-1))  min_child=left;
+  //J: has left child only
+  if(left < (size()-1) && right > (size()-1))  min_child=left;
 
-	//J: has both children
-	else min_child= heap[left].key < heap[right].key ? left : right;
+  //J: has both children
+  else min_child= heap[left].key < heap[right].key ? left : right;
   }
 }*/
 
+// Need to remove
 template <class T, class K>
-void BinaryHeap<T,K>::popMin(){
-	if(size()==0)
-	{
-		return;
-	}
-	
-	swap(heap[0],heap[size()-1]);
-	heap.pop_back();
-	fixHeapDown(0);
+void BinaryHeap<T, K>::insert(const T& item, const K& key, vector<HeapItem<T,K>>& heapcopy, const unsigned int heapsize){
+  HeapItem<T, K> temp_v;  // Need to update this approach
+  temp_v.item = item;
+  temp_v.key = key;
+  heapcopy.push_back(temp_v);
+  // R: Should be implemented in fixheapup
+//  unsigned int v_i = heapcopy.size() - 1;
+    unsigned int v_i = heapsize;
+  unsigned int p_i = parent_index(v_i);
+  while ((v_i != 0) && (heapcopy[v_i].key < heapcopy[p_i].key)){
+    swap(heapcopy[v_i], heapcopy[p_i]);
+    v_i = p_i;
+    p_i = parent_index(v_i);
+  }
 }
+
+template <class T, class K>
+void BinaryHeap<T, K>::popMin(){
+
+    std::vector< HeapItem<T, K> > heapcopy;
+
+    for (int i = 1; i < heap.size(); i++){
+        HeapItem<T, K> temp;
+        temp.item = heap[i].item;
+        temp.key = heap[i].key;
+        insert(temp.item, temp.key, heapcopy, heapcopy.size());
+    }
+    heap = heapcopy;
+    return;
+}
+
+
+// template <class T, class K>
+// void BinaryHeap<T,K>::popMin(){
+//   if(size()==0)
+//   {
+//     return;
+//   }
+  
+//   swap(heap[0],heap[size()-1]);
+//   heap.pop_back();
+//   fixHeapDown(0);
+// }
+
+
+template <class T, class K>
+unsigned int BinaryHeap<T, K>::parent_index(unsigned int i){
+  if (i > 0){
+    // R: Since we are storing all the values in array
+    return ((i-1)/2);
+  }
+  else
+    return 0;
+}
+
+
+
+// The origianl implementation
+// #include <vector>
+
+// // Only assumes the key type K is totally ordered and comparable via <
+// template <class T, class K>
+// struct HeapItem {
+//   T item;
+//   K key;
+// };
+
+// template <class T, class K>
+// class BinaryHeap {
+// public:
+//   // constructor not required because the only "initialization"
+//   // is through the constructor of the variable "heap" which is called by default
+
+//   // return the minimum element in the heap
+//   HeapItem<T, K> min() const;
+
+//   // insert an item with the given key
+//   // if the item is already in the heap, will still insert a new copy with this key
+//   void insert(const T& item, const K& key);
+
+//   // pop the minimum item from the heap
+//   void popMin();
+
+//   // returns the number of items held in the heap
+//   int size() const;
+
+// private:
+//   // the array holding the heap
+//   std::vector< HeapItem<T, K> > heap;
+
+//   // will return the left child of the vertex
+//   int left(int parent);
+
+//   //will return the right child of the vertex
+//   int right(int parent);
+
+//   //  will fix the heap property at index i and recurse with its parent
+//   void fixHeapUp(int i);
+
+//   // will fix the heap property at index i and recurse with the child
+//   // that received i's item (if appropriate)
+//   void fixHeapDown(int i);
+
+//   // R: Returns the parent index of the given index (Helper method)
+//   // unsigned int parent_index(unsigned int i);
+// };
+
+// /*
+//   R: Returns the pair (x, key) with the smallest key
+// */
+// template <class T, class K>
+// HeapItem<T, K> BinaryHeap<T, K>::min() const{
+//   if (heap.size() > 0){
+//     // R: Since the first element of the array holding the heap is the root index
+//     // R: Which holds the minimum key value
+//     return heap[0];
+//   }
+//   //return ;
+// }
+
+// template <class T, class K>
+// void BinaryHeap<T, K>::fixHeapUp(int i){
+
+//     int p_i = (i-1)/2;
+//     // R: If the vertex is at root then no need to check for heap property
+//     // R; If the new vertex validates the heap property if not swap
+//     if ((i != 0) && (heap[i].key < heap[p_i].key)){
+//         swap(heap[i], heap[p_i]);
+//         i = p_i;
+//         // R: Checkng if the swaped vertex obey's the heap property
+//         fixHeapUp(i);
+//     }
+//     // R: Base case if it's satisfies the heap property or vertex is at root
+//     else
+//         return;
+// }
+
+// template <class T, class K>
+// void BinaryHeap<T, K>::insert(const T& item, const K& key){
+//   HeapItem<T, K> temp_v;  // Need to update this approach
+//   temp_v.item = item;
+//   temp_v.key = key;
+//   heap.push_back(temp_v);
+//   // R: Should be implemented in fixheapup
+//   unsigned int v_i = heap.size() - 1;
+//   fixHeapUp(v_i);
+//   // unsigned int p_i = parent_index(v_i);
+//   // while ((v_i != 0) && (heap[v_i].key < heap[p_i].key)){
+//   //   swap(heap[v_i], heap[p_i]);
+//   //   v_i = p_i;
+//   //   p_i = parent_index(v_i);
+//   // }
+// }
+
+// template <class T, class K>
+// int BinaryHeap<T, K>::size() const{
+//   return heap.size();
+// }
+
+// // template <class T, class K>
+// // unsigned int BinaryHeap<T, K>::parent_index(unsigned int i){
+// //   if (i > 0){
+// //     // R: Since we are storing all the values in array
+// //     return ((i-1)/2);
+// //   }
+// //   else
+// //     return 0;
+// // }
+
+// template <class T, class K>
+// int BinaryHeap<T, K>::left(int parent){
+// 	int left=2*parent+1;
+
+// 	//J: if the left child has a valid index
+// 	if (left < size())
+// 		return left;
+// 	//J: if the index of left child is out of bounds
+// 	else
+// 		return -1;
+// }
+// template <class T, class K>
+// int BinaryHeap<T, K>::right(int parent){
+// 	int right=2*parent+1;
+
+// 	//J: if the right child has a valid index
+// 	if (right < size())
+// 		return right;
+// 	//J: if the index of right child is out of bounds
+// 	else
+// 		return -1;
+// }
+// template <class T, class K>
+// void BinaryHeap<T,K>::fixHeapDown(int i)
+// {
+// 	int child1=left(i);
+// 	int child2=right(i);
+	
+// 	//J: if both the children are valid indices and the right child has the minimum value
+// 	if(child1 >= 0 && child2 >= 0 && heap[child1].key > heap[child2].key)
+// 	{
+// 		child1=child2;
+// 	}
+// 	//J: if the minimum child is a valid index and the does not obey the heap property,swap
+// 	if (child1 > 0 && heap[i].key > heap[child1].key)
+// 	{
+// 		swap(heap[i], heap[child1]);
+// 		//J: checking if the swapped vertex obeys the heap property
+// 		fixHeapDown(child1);
+// 	}
+// }
+
+//   /*while (heap[i].key > heap[min_child].key)
+//   {
+
+//   //J: has no children
+//   if(left> size()-1)  break;
+
+//     swap(heap[i], heap[min_child]);
+//     i=min_child;
+//     left=2*i+1;
+//   	right=2*i+2;
+
+// 	// //J: has no children
+// 	// if(left> size()-1)  break;
+
+// 	//J: has left child only
+// 	if(left < (size()-1) && right > (size()-1))  min_child=left;
+
+// 	//J: has both children
+// 	else min_child= heap[left].key < heap[right].key ? left : right;
+//   }
+// }*/
+
+// template <class T, class K>
+// void BinaryHeap<T,K>::popMin(){
+// 	if(size()==0)
+// 	{
+// 		return;
+// 	}
+	
+// 	swap(heap[0],heap[size()-1]);
+// 	heap.pop_back();
+// 	fixHeapDown(0);
+// }
 
 
--- part1_starter/server.cpp
+++ part1_starter/server.cpp
@@ -5,14 +5,10 @@
 //#include "digraph.h"
 #include "dijkstra.h"
 #include <fstream>
-#include <list> // Need to update this
+#include <list> // Need to remove this
+#include <vector> // Need to remove this
 
 using namespace std;
-
-// R: Alias for pair<int, long long>
-typedef pair<int, long long> PIL;
-
-// R: added this template file to make things easier
 
 struct Point 
 {
@@ -27,9 +23,7 @@ long long manhattan(const Point& pt1,const Point& pt2)
     return manDist;
 }
 
-void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& points) 
-{
-    /*
+/*
     Read the Edmonton map data from the provided
     fileand load it into the given WDigraph object.
     Store vertex coordinates in Point struct and map
@@ -40,6 +34,8 @@ void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& poin
     graph: an instance of the weighted directed graph (WDigraph)class
     points: a mapping between vertex identifiers and their coordinates
 */
+void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& points) 
+{
 	ifstream fin;
 	fin.open(filename);
 
@@ -53,6 +49,7 @@ void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& poin
 	    string input_line,value; char V = 'V', E = 'E', comma = ',';
 	    //reading a line from stdin using istream object cin
 	    getline(fin, input_line);
+	    // cout << input_line << endl;
 	    //storing the choice, 'V' or 'E'
 	    int pos = input_line.find(",");
 	    // value = input_line.substr(0,pos);
@@ -68,6 +65,7 @@ void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& poin
 	      int v=stoi(temp);
 	      // add the vertex to the graph
 	      graph.addVertex(v);
+	      // cout << "Added vertex: "<< v << endl;
 
 	      input_line=input_line.substr(pos+1);
 	      pos=input_line.find(",");
@@ -105,9 +103,21 @@ void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& poin
       		graph.addEdge(ver1, ver2, weight);
     	}
   	}
+
   	fin.close();
   	return;
 }
+
+
+// int main()
+// {
+// 	WDigraph wgraph;
+// 	string filename="edmonton-roads-2.0.1.txt";
+// 	//Point locations;
+// 	unordered_map<int, Point> points;
+// 	readGraph(filename, wgraph, points);
+// 	return 0;
+// }
 
 
 int main()
@@ -121,29 +131,40 @@ int main()
 	Point start_point, end_point;
 	string temp;
 	cin >> temp;
-	if (temp[0] == 'R'){
+	if (temp == "R"){
 		cin >> temp;
 		start_point.lat = stol(temp);
+		cout << start_point.lat << endl;
 		cin >> temp;
 		start_point.lon = stol(temp);
+		cout << start_point.lon << endl;
 		cin >> temp;
 		end_point.lat = stol(temp);
+		cout << end_point.lat << endl;
 		cin >> temp;
 		end_point.lon = stol(temp);
+		cout << end_point.lon << endl;
 	}
-	int startVertex = 0, endVertex = 0;
+	int startVertex = 277483195, endVertex = 561041122;
+	// int startVertex = 0, endVertex = 0;
 
-	for (auto it: points){
-		if (it.second.lat == start_point.lat){
-			startVertex = it.first;
-		}
-	}
+	// for (auto it: points){
+	// 	// cout << it.first << endl;
+	// 	// cout << it.second.lat << " and " << it.second.lon << endl;
+	// 	if ((it.second.lat == start_point.lat)){
+	// 		startVertex = it.first;
+	// 		cout << it.second.lat << " and " << it.second.lon << endl;
+	// 	}
+	// }
+	// cout << startVertex << endl;
 
-	for (auto it: points){
-		if (it.second.lat == end_point.lat){
-			endVertex = it.first;
-		}
-	}
+	// for (auto it: points){
+	// 	if ((it.second.lat == end_point.lat)){
+	// 		endVertex = it.first;
+	// 		cout << it.second.lat << " and " << it.second.lon << endl;
+	// 	}
+	// }
+	// cout << endVertex << endl;
 
 	unordered_map<int, PIL> searchTree;
 	dijkstra(wgraph, startVertex, searchTree);
@@ -156,7 +177,6 @@ int main()
 		int stepping = endVertex;
 		while (stepping != startVertex) {
 			path.push_front(stepping);
-
 			stepping = searchTree[stepping].first;
 		}
 		path.push_front(startVertex);
@@ -172,3 +192,182 @@ int main()
 	cout << "E" << endl;
 	return 0;
 }
+
+
+
+// The origianl implementation
+
+// #include <iostream>
+// #include <cstdlib>
+// #include "heap.h"
+// #include "wdigraph.h"
+// //#include "digraph.h"
+// #include "dijkstra.h"
+// #include <fstream>
+// #include <list> // Need to update this
+
+// using namespace std;
+
+// // R: Alias for pair<int, long long>
+// typedef pair<int, long long> PIL;
+
+// // R: added this template file to make things easier
+
+// struct Point 
+// {
+//     long long lat; // Latitude of the point
+//     long long lon; // Longitude of the point
+// };
+
+// long long manhattan(const Point& pt1,const Point& pt2) 
+// {
+//     // Computes and returns the Manhattan distance between the two given points
+//     long long manDist= abs(pt1.lat-pt2.lat)+abs(pt1.lon-pt2.lon);
+//     return manDist;
+// }
+
+// void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& points) 
+// {
+//     /*
+//     Read the Edmonton map data from the provided
+//     fileand load it into the given WDigraph object.
+//     Store vertex coordinates in Point struct and map
+//     each vertex to its corresponding Point struct.
+
+//     PARAMETERS:
+//     filename: name of the file describing a road network
+//     graph: an instance of the weighted directed graph (WDigraph)class
+//     points: a mapping between vertex identifiers and their coordinates
+// */
+// 	ifstream fin;
+// 	fin.open(filename);
+
+// 	// R: I changed the name of variable str to input_line to
+// 	//increase readability
+// 	while(!fin.eof())
+//   	{// a loop to read from the stdin until EOF is reached
+
+// 	    // declaring variables
+// 	    Point pt;
+// 	    string input_line,value; char V = 'V', E = 'E', comma = ',';
+// 	    //reading a line from stdin using istream object cin
+// 	    getline(fin, input_line);
+// 	    //storing the choice, 'V' or 'E'
+// 	    int pos = input_line.find(",");
+// 	    // value = input_line.substr(0,pos);
+// 	    // input_line = input_line.substr(pos+1);
+
+// 	    if(input_line[0] == V)
+// 	    {// if a vertex is given
+// 	      string temp;
+// 	      input_line = input_line.substr(pos+1);
+// 	      //read and store the ID of the vertex
+// 	      pos=input_line.find(",");
+// 	      temp=input_line.substr(0,pos);
+// 	      int v=stoi(temp);
+// 	      // add the vertex to the graph
+// 	      graph.addVertex(v);
+
+// 	      input_line=input_line.substr(pos+1);
+// 	      pos=input_line.find(",");
+// 	      temp = input_line.substr(0,pos);
+// 	      double coord= stod(temp);
+// 	      pt.lat=static_cast<long long>(coord*100000);
+
+// 	      temp =input_line.substr(pos+1);
+// 	      coord= stod(temp);
+// 	      pt.lon=static_cast<long long>(coord*100000);
+// 	      points[v]=pt;
+// 	    }
+//     	// R: Since the start of the input line would either be 'V' or 'E'
+//     	else if (input_line[0] == E){
+//       		int bounds = input_line.size()+1;
+//       		int index = 2, substr_i = 0; char temp = input_line[index];
+//       		string temp_string = "", sub_str[2];
+//       		for (int i = index; i < bounds; i++) {
+//         		temp = input_line[i];
+//         		if (temp == comma){
+//           			sub_str[substr_i] = temp_string;
+//           			temp_string = "";
+//           			substr_i++;
+//           			continue;
+//         		}
+//         		else{
+//           			temp_string += temp;
+//         		}
+//       		}
+//       		int ver1 = stoi(sub_str[0]);
+//       		int ver2 = stoi(sub_str[1]);
+
+//       		//calculating and adding the weight for this edge
+//       		long long weight= manhattan(points[ver1],points[ver2]);
+//       		graph.addEdge(ver1, ver2, weight);
+//     	}
+//   	}
+//   	fin.close();
+//   	return;
+// }
+
+
+// int main()
+// {
+// 	WDigraph wgraph;
+// 	string filename="edmonton-roads-2.0.1.txt";
+// 	//Point locations;
+// 	unordered_map<int, Point> points;
+// 	readGraph(filename, wgraph, points);
+
+// 	Point start_point, end_point;
+// 	string temp;
+// 	cin >> temp;
+// 	if (temp[0] == 'R'){
+// 		cin >> temp;
+// 		start_point.lat = stol(temp);
+// 		cin >> temp;
+// 		start_point.lon = stol(temp);
+// 		cin >> temp;
+// 		end_point.lat = stol(temp);
+// 		cin >> temp;
+// 		end_point.lon = stol(temp);
+// 	}
+// 	int startVertex = 0, endVertex = 0;
+
+// 	for (auto it: points){
+// 		if (it.second.lat == start_point.lat){
+// 			startVertex = it.first;
+// 		}
+// 	}
+
+// 	for (auto it: points){
+// 		if (it.second.lat == end_point.lat){
+// 			endVertex = it.first;
+// 		}
+// 	}
+
+// 	unordered_map<int, PIL> searchTree;
+// 	dijkstra(wgraph, startVertex, searchTree);
+
+// 	list<int> path;
+// 	if (searchTree.find(endVertex) == searchTree.end()){
+// 		cout << "N " << 0 << endl;
+// 	}
+// 	else {
+// 		int stepping = endVertex;
+// 		while (stepping != startVertex) {
+// 			path.push_front(stepping);
+
+// 			stepping = searchTree[stepping].first;
+// 		}
+// 		path.push_front(startVertex);
+// 	}
+// 	cout << "N " << path.size();
+// 	for (auto it: path){
+// 		cin >> temp;
+// 		if (temp[0] == 'A'){
+// 			cout << "W " << points[it].lat << " " << points[it].lon;
+// 		}
+// 	}
+
+// 	cout << "E" << endl;
+// 	return 0;
+// }
